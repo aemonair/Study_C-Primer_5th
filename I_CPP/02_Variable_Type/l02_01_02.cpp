c498797756b8e2fe39f60f6e620ff3cdad639922
Aemonair <v.aemonair@gmail.com>
2017-10-15 07:22:29 +0800

    l02_01_02

--- I_CPP/02_Variable_Type/l02_01_02.cpp
+++ I_CPP/02_Variable_Type/l02_01_02.cpp
@@ -3,19 +3,32 @@
 
 int main()
 {
+    std::cout << "练习2.3 读程序写结果："                                  << std::endl;
+    std::cout << "unsigned u = 10, u2 = 42; "                              << std::endl;
+    std::cout << "std::cout << u2 - u  << std::endl;   // 32 "             << std::endl;
+    std::cout << "std::cout << u  - u2 << std::endl;   // 2^32 + 10 - 42 " << std::endl;
+
+    std::cout << "int i = 10, i2 = 42; "                                   << std::endl;
+    std::cout << "std::cout << i2 - i  << std::endl;   // 32 "             << std::endl;
+    std::cout << "std::cout << i  - i2 << std::endl;   // -32 "            << std::endl;
+    std::cout << "std::cout << i  - u  << std::endl;   // 0 "              << std::endl;
+    std::cout << "std::cout << u  - i  << std::endl;   // 0 "              << std::endl;
+
+    std::cout << "----------------------" << std::endl;
+
     unsigned u = 10, u2 = 42;
-    std::cout << u2 - u  << std::endl;   // 32
-    std::cout << u  - u2 << std::endl;   // 2^32 + 10 - 42
+    std::cout << u2 - u  << std::endl;                 // 32
+    std::cout << u  - u2 << std::endl;                 // 2^32 + 10 - 42  //  4294967264 
 
     int i = 10, i2 = 42;
-    std::cout << i2 - i  << std::endl;   // 32
-    std::cout << i  - i2 << std::endl;   // -32
-    std::cout << i  - u  << std::endl;   // 0
-    std::cout << u  - i  << std::endl;   // 2^32
+    std::cout << i2 - i  << std::endl;                 // 32
+    std::cout << i  - i2 << std::endl;                 // -32
+    std::cout << i  - u  << std::endl;                 // 0
+    std::cout << u  - i  << std::endl;                 // 2^32 wrong: (0)
  
-    std::cout << std::endl;
+    std::cout << "----------------------" << std::endl;
     //std::cout << int(pow(2, 32)) - 42 + 10 << std::endl;
-    std::cout << 4294967296 - 42 + 10 << std::endl;
+    //std::cout << 4294967296 - 42 + 10 << std::endl;
     return 0;
 }
 
@@ -26,5 +39,6 @@ int main()
 //  -32 
 //  0 
 //  0
-//  4294967296
+//  4294967264 
+//  2^32 == 4294967296
 
