f5341169fa02af73ab1c7fa51e44e78a34b4b710
Aemonair <v.aemonair@gmail.com>
2017-10-15 07:23:04 +0800

    l02_02_02

--- I_CPP/02_Variable_Type/l02_02_02.cpp
+++ I_CPP/02_Variable_Type/l02_02_02.cpp
@@ -1,7 +1,13 @@
-// 练习 2.11 指出下面的语句是声明还是定义：
-// (a)
-    extern int ix = 1024;
-// (b)
-        int iy;
-// (c)
-extern int iz;
+#include <iostream>
+
+int main()
+{
+    std::cout << "练习 2.11 指出下面的语句是声明还是定义：" << std::endl;
+    std::cout << "(a) extern int ix = 1024;" << std::endl;
+    std::cout << "> 定义" << std::endl;
+    std::cout << "(b)int iy;" << std::endl;
+    std::cout << "> 定义" << std::endl;
+    std::cout << "(c)extern int iz;" << std::endl;
+    std::cout << "> 声明" << std::endl;
+    return 0;
+}
