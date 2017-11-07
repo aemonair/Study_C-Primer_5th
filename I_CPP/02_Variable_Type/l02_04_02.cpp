b2f338c1924deffe673fe35e6ddfefcc9de3a790
Aemonair <v.aemonair@gmail.com>
2017-11-08 00:11:57 +0800

    ltest02_04_02

---
+++ I_CPP/02_Variable_Type/l02_04_02.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "2.4.2节练习" << std::endl;
+    std::cout << "下面的哪些初始化是合法的？请说明原因。" << std::endl;
+    std::cout << "(a) int i = -1, &r = 0;" << std::endl;
+    std::cout << "    error: invalid initialization of non-const reference of type ‘int&’ from an rvalue of type ‘int’" << std::endl;
+    std::cout << "        int i = -1, &r = 0;" << std::endl;
+    std::cout << "                         ^" <<std::endl;
+    std::cout << "(b) int *cosnt p2 = &i2;" << std::endl;
+    std::cout << "(c) const int i = -1, &r = 0;" << std::endl;
+    std::cout << "(d) const int *const p3 = &i2;" << std::endl;
+    std::cout << "(e) const int *p1 = &i2;" << std::endl;
+    std::cout << "(f) const int &const r2;" << std::endl;
+    std::cout << "    error: ‘const’ qualifiers cannot be applied to ‘const int&’" << std::endl;
+    std::cout << "        const int &const r2;" << std::endl;
+    std::cout << "                         ^" << std::endl;
+    std::cout << "    error: ‘r2’ declared as reference but not initialized" << std::endl;
+    std::cout << "(g) const int i2 = i, &r = i;" << std::endl;
+
+    std::cout << "-----------------------------------" << std::endl;
+    std::cout << "说明下面的这些定义是什么意思，挑出其中不合法的。" << std::endl;
+
+    std::cout << "(a) int i, *const cp;" << std::endl;
+    std::cout << "error: uninitialized const ‘cp’ [-fpermissive]" << std::endl;
+    std::cout << "     int i, *const cp;" << std::endl;
+    std::cout << "                   ^" << std::endl;
+    std::cout << "(b) int *p1, *const p2;" << std::endl;
+    std::cout << "error: uninitialized const ‘p2’ [-fpermissive]" << std::endl;
+    std::cout << "     int *p1, *const p2;" << std::endl;
+    std::cout << "                     ^" << std::endl;
+    std::cout << "(c) const int ic, &r = ic;" << std::endl;
+    std::cout << "error: uninitialized const ‘ic’ [-fpermissive]" << std::endl;
+    std::cout << "     const int ic, &r = ic;" << std::endl;
+    std::cout << "               ^" << std::endl;
+    std::cout << "(d) const int *const p3;" << std::endl;
+    std::cout << "error: uninitialized const ‘p3’ [-fpermissive]" << std::endl;
+    std::cout << "     const int *const p3;" << std::endl;
+    std::cout << "                      ^" << std::endl;
+    std::cout << "(e) const int *p;" << std::endl;
+    std::cout << " not wrong" << std::endl;
+
+    std::cout << "-----------------------------------" << std::endl;
+    std::cout << "练习2.29 假设已有上一个练习中定义的那些变量，则下面的哪些语句是合法的？请说明原因。" << std::endl;
+
+    std::cout << "(a) i = ic;" << std::endl;
+    std::cout << "(b) p1 = p3;" << std::endl;
+    std::cout << "(c) p1 = &ic;" << std::endl;
+    std::cout << "(d) p3 = &ic;" << std::endl;
+    std::cout << "(e) p2 = p1;" << std::endl;
+    std::cout << "(f) ic = *p3;" << std::endl;
+
+    return 0;
+}
